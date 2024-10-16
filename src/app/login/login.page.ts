282fd4d57fc92ad7d6e979d32f9a30054f0cb2f6
Edilson Bitencourt <theusdido@hotmail.com>
2024-10-16 16:19:35 -0300

    fanalisado tela de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,3 +14,4 @@ export class LoginPage implements OnInit {
   }
 
 }
+
